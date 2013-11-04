58d2b49d84ce7ffd16f1de397d3c347a95f973d8
Matt Clarke <matt.clarke@stfc.ac.uk>
2013-11-04 15:16:55 +0000

    Refs ticket 221. Corrected mistakes I made in some makefiles due to using an old version of lvDcom
    Former-commit-id: 633c0b8b63d5d7ff05bda436ccda25792d76eaf7

--- iocBoot/iocAgilent_53220A/st.cmd
+++ iocBoot/iocAgilent_53220A/st.cmd
@@ -26,11 +26,11 @@ cd ${TOP}/iocBoot/${IOC}
 ##
 ## the "options" argument is a combination of the following flags (as per the #lvDCOMOptions enum in lvDCOMInterface.h)
 ##    viWarnIfIdle=1, viStartIfIdle=2, viStopOnExitIfStarted=4, viAlwaysStopOnExit=8
-lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_53220AApp/protocol/agilent53200A.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
-#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_53220AApp/protocol/agilent53200A.xml", "", 6)
+#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_53220AApp/protocol/agilent53200A.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
+lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_53220AApp/protocol/agilent53200A.xml", "", 6)
 
 dbLoadRecords("$(TOP)/db/Agilent_53220A.db","P=$(IOCNAME):")
-dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
+#dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
 
 #asynSetTraceMask("frontpanel",0,0xff)
 asynSetTraceIOMask("frontpanel",0,0x2)
